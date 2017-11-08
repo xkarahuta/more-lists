085a281624e6694d06c2813ad6c495fc8c7db0d0
xkarahuta <xkarahuta@ucvts.tec.nj.us>
2017-11-08 15:02:27 +0000

    updated list-practice

--- list-practice.py
+++ list-practice.py
@@ -6,7 +6,7 @@ If you're correct, you'll get a message in the terminal saying "Your string_list
 
 # On the line below, create a list named string_list containing 7 strings
 
-
+string_list = ["help", "me", "sweet", "jesus", "christ", "gotta", "finish"]
 
 
 
